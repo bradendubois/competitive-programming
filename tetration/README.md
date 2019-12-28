282b878c57930d42a205684aa22712c865dae8c0
Braden <drab3n@gmail.com>
2019-12-27 18:46:13 -0600

    Added tetration solution

---
+++ tetration/README.md
@@ -0,0 +1,9 @@
+# Tetration
+
+**Problem**: [tetration](https://open.kattis.com/problems/tetration)
+
+**Difficulty**: 1.7
+
+## Solution
+
+Simply raise *N* to the power of (1 / *N*).
---
+++ tetration/tetration.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double N;
+    cin >> N;
+    cout << pow(N, (1/N)) << endl;
+}
