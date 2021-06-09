9a601ebb032d7b61b53c2ca2c8780ba19ed445b3
Braden Dubois <braden.dubois@usask.ca>
2021-06-09 14:24:35 -0600

    improve wiki builder

--- build_wiki.py
+++ build_wiki.py
@@ -1,3 +1,4 @@
+from kattis_download.main import get_soup, parse_soup
 from pathlib import Path
 
 p = Path(".")
@@ -18,8 +19,18 @@ for directory in filter(lambda x: x.is_dir() and not x.name.startswith(".") and 
     with readme.open("r") as f:
         contents = f.read().strip()
 
+    data = parse_soup(get_soup(directory))
+
     full_contents = f"\
-        # {directory.name}\n\
+        # {data['title']}\n\
+        \n\
+        ID: {directory.name}\n\
+        \n\
+        Difficulty: {data['difficulty']}\n\
+        \n\
+        CPU Time: {data['cpu']}\n\
+        \n\
+        Memory: {data['memory']}\n\
         \n\
         ## Solution\n\
         \n\
