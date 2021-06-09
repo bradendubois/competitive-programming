f44f938cbf095659749ae6bf0ecaa3df5c87048d
Braden Dubois <braden.dubois@usask.ca>
2021-06-09 14:40:34 -0600

    fix: fix formatting from resulting script

--- build_wiki.py
+++ build_wiki.py
@@ -21,21 +21,12 @@ for directory in filter(lambda x: x.is_dir() and not x.name.startswith(".") and 
 
     data = parse_soup(get_soup(directory.name))
 
-    full_contents = f"\
-        # {data['title']}\n\
-        \n\
-        ID: {directory.name}\n\
-        \n\
-        Difficulty: {data['difficulty']}\n\
-        \n\
-        CPU Time: {data['cpu']}\n\
-        \n\
-        Memory: {data['memory']}\n\
-        \n\
-        ## Solution\n\
-        \n\
-        {contents}\
-        "
+    full_contents = f"# {data['title']}\n\n\
+    ID: {directory.name}\n\n\
+    Difficulty: {data['difficulty']}\n\n\
+    CPU Time: {data['cpu']}\n\n\
+    Memory: {data['memory']}\n\n\
+    ## Solution\n\n\{contents}"
 
     page = (wiki / (directory.name + ".md"))
     page.touch(0o777)
